b8d10b6ba81e080dd198496880e2a268538f334f
atharvnaik1 <atharvnaik1@users.noreply.github.com>
2026-04-24 00:04:39 +0530

    ok

--- wrappers/ionic/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Capacitor } from '@capacitor/core';
-
-export class IPAShipIonic {
-  static audit(filePath: string) {
-    console.log(`Auditing ${filePath} in Ionic/Capacitor via ipaship.com...`);
-  }
-}
